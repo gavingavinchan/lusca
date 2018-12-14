f17655ede9e366e41f876a1a3e3e5b4ae3213178
gavingavinchan <gavingavinchan@gmail.com>
2018-12-14 20:57:16 +0800

    testing calibration values from video

--- computerVision/camCalibration/calibration from DMI/undistortExternalValues.py
+++ computerVision/camCalibration/calibration from DMI/undistortExternalValues.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-cvFile = cv.FileStorage("calibrationValues.xml",cv.FILE_STORAGE_READ)
+cvFile = cv.FileStorage("calibrationValuesVideo.xml",cv.FILE_STORAGE_READ)
 mtx = cvFile.getNode("mtx").mat()
 dist = cvFile.getNode("dist").mat()
 newCameraMTX = cvFile.getNode("newCameraMTX").mat()
